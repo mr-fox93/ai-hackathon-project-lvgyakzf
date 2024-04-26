163c5c572787b7e0af67dee8dee0f18f369d1f26
The Fox <lisieckikamil93@gmail.com>
2024-04-26 22:24:27 +0200

    feat: setup speechtotext api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-// src/App.tsx
 import React, { useState } from "react";
 import { fetchChatCompletion } from "./apiService";
+import { saveAs } from "file-saver";
+import SpeechToText from "./SpeechTotext";
 
 const App: React.FC = () => {
   const [input, setInput] = useState("");
   const [response, setResponse] = useState({});
+
+  const handleTranscription = (transcript: string) => {
+    setInput(transcript);
+  };
 
   const handleSubmit = async () => {
     const result = await fetchChatCompletion(input);
@@ -14,13 +19,14 @@ const App: React.FC = () => {
 
   return (
     <div>
+      <SpeechToText onTranscript={handleTranscription} />
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
       <button onClick={handleSubmit}>Send</button>
-      <pre>{JSON.stringify(response, null, 2)}</pre>{" "}
+      <pre>{JSON.stringify(response, null, 2)}</pre>
     </div>
   );
 };
