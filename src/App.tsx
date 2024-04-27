4ca6a8ad0e08835b1bd6b0c865dc9c5e102a55d1
wawrzonnnn <norbert.wawrzonek@gmail.com>
2024-04-27 20:59:32 +0200

    fix: resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,127 +1,172 @@
-import React, { useEffect } from "react";
-import { fetchChatCompletion, createPromptForOpenAI } from "./apiService";
-import styles from "./app.module.css";
-import { addProduct, getProducts, removeProduct } from "./database";
-import { useStore } from "./store/useStore";
-import SpeechToText from "./SpeechTotext";
+import React, { useEffect } from 'react'
+import { fetchChatCompletion, createPromptForOpenAI } from './apiService'
+import styles from './app.module.css'
+import { addProduct, getProducts, removeProduct, deleteDatabase } from './database'
+import { useStore } from './store/useStore'
+import SpeechToText from './SpeechTotext'
+import { knownProducts } from './knownProducts'
 
 const App: React.FC = () => {
-  const {
-    inputProduct,
-    setInputProduct,
-    products,
-    setProducts,
-    showQuickMealPanel,
-    setShowQuickMealPanel,
-    quickMealInput,
-    setQuickMealInput,
-    setResult,
-    results,
-  } = useStore();
+	const {
+		inputProduct,
+		setInputProduct,
+		products,
+		setProducts,
+		showQuickMealPanel,
+		setShowQuickMealPanel,
+		quickMealInput,
+		setQuickMealInput,
+		setResult,
+		results,
+		showMealPlan,
+		setShowMealPlan,
+		showPantry,
+		setShowPantry,
+	} = useStore()
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  const normalizeText = (text: string) =>
+		text
+			.normalize('NFD')
+			.replace(/[\u0300-\u036f]/g, '')
+			.toLowerCase()
 
-  useEffect(() => {
-    console.log(results);
-  }, [results]);
+	useEffect(() => {
+		fetchProducts()
+	}, [])
 
-  const fetchProducts = async () => {
-    const items = await getProducts();
-    setProducts(items);
-  };
+	useEffect(() => {
+		console.log(results)
+	}, [results])
 
-  const handleAddProduct = async () => {
-    if (inputProduct.trim() !== "") {
-      const newProduct = await addProduct({ name: inputProduct });
-      if (newProduct.id) {
-        setProducts((prevProducts) => [...prevProducts, newProduct]);
-        setInputProduct("");
-      }
-    }
-  };
+	const fetchProducts = async () => {
+		const items = await getProducts()
+		setProducts(items)
+	}
 
-  const handleDeleteProduct = async (id: IDBValidKey | IDBKeyRange) => {
-    await removeProduct(id);
-    fetchProducts();
-  };
+const handleAddProduct = async () => {
+		let inputLower = normalizeText(inputProduct)
+		const productsToAdd = []
 
-  const toggleQuickMealPanel = () => {
-    setShowQuickMealPanel(!showQuickMealPanel);
-    setQuickMealInput(inputProduct);
-  };
+		// Check for known products and add them separately
+		for (const product of knownProducts) {
+			const normalizedProduct = normalizeText(product)
+			if (inputLower.includes(normalizedProduct)) {
+				productsToAdd.push(product) // Add the original name, not normalized
+				inputLower = inputLower.replace(new RegExp(normalizedProduct, 'g'), '')
+			}
+		}
 
-  const handleTranscription = (transcript: string) => {
-    setInputProduct(transcript);
-  };
+		// Add remaining single word products
+		inputLower.split(' ').forEach(word => {
+			if (word.trim() !== '') productsToAdd.push(word)
+		})
 
-  const handleClear = () => {
-    setInputProduct("");
-  };
+		// Add each product to the database and state
+		for (const product of productsToAdd) {
+			const newProduct = await addProduct({ name: product })
+			if (newProduct.id) {
+				setProducts(prev => [...prev, newProduct])
+			}
+		}
 
-  const handleGenerateMeal = async () => {
-    const prompt = createPromptForOpenAI(quickMealInput);
-    const response = await fetchChatCompletion(prompt);
-    console.log(response); //Tutaj możesz przetworzyć odpowiedź, np. wyświetlić ją użytkownikowi
-    setResult(response);
-  };
+		setInputProduct('')
+	}
 
-  return (
+
+	const handleDeleteProduct = async (id: IDBValidKey | IDBKeyRange) => {
+		await removeProduct(id)
+		fetchProducts()
+	}
+
+	const toggleQuickMealPanel = () => {
+		setShowQuickMealPanel(!showQuickMealPanel)
+		setQuickMealInput(inputProduct)
+	}
+
+	const handleTranscription = (transcript: string) => {
+		setInputProduct(transcript)
+	}
+
+	const handleClear = () => {
+		setInputProduct('')
+	}
+
+	const handleGenerateMeal = async () => {
+		const prompt = createPromptForOpenAI(quickMealInput)
+		const response = await fetchChatCompletion(prompt)
+		console.log(response) //Tutaj możesz przetworzyć odpowiedź, np. wyświetlić ją użytkownikowi
+		setResult(response)
+	}
+
+  const togglePantry = () => {
+		setShowPantry(!showPantry)
+	}
+
+	const toggleMealPlan = () => {
+		setShowMealPlan(!showMealPlan)
+	}
+
+
+	return (
     <div className={styles.container}>
+    {!showPantry && !showMealPlan && !showQuickMealPanel && (
       <div className={styles.actionsWrapper}>
-        <button>SPICHLERZ</button>
-        <button>JADŁOSPISY</button>
+        <button onClick={togglePantry}>SPICHLERZ</button>
+        <button onClick={toggleMealPlan}>JADŁOSPISY</button>
       </div>
-      {!showQuickMealPanel && (
-        <div className={styles.mainWrapper}>
-          <SpeechToText
-            onTranscript={handleTranscription}
-            onClear={handleClear}
-          />
+    )}
 
-          <textarea
-            className={styles.textarea}
-            value={inputProduct}
-            onChange={(e) => setInputProduct(e.target.value)}
-            placeholder="Enter products..."
-            style={{ width: "100%", minHeight: "50px" }}
-          />
-          <div className={styles.buttonsWrapper}>
-            <button onClick={handleAddProduct}>DODAJ DO SPICHLERZA</button>
-            <button onClick={toggleQuickMealPanel}>SZYBKIE JEDZONKO</button>
-          </div>
+    {!showPantry && !showMealPlan && !showQuickMealPanel && (
+      <div className={styles.mainWrapper}>
+  <SpeechToText onTranscript={handleTranscription} onClear={handleClear} />
+        <textarea
+          className={styles.textarea}
+          value={inputProduct}
+          onChange={e => setInputProduct(e.target.value)}
+          placeholder='Enter products...'
+        />
+        <div className={styles.buttonsWrapper}>
+          <button onClick={handleAddProduct}>DODAJ DO SPICHLERZA</button>
+          <button onClick={toggleQuickMealPanel}>SZYBKIE JEDZONKO</button>
         </div>
-      )}
+      </div>
+    )}
 
-      {showQuickMealPanel && (
-        <div className={styles.popupWrapper}>
-          <textarea
-            className={styles.popupTextarea}
-            value={quickMealInput}
-            onChange={(e) => setQuickMealInput(e.target.value)}
-          />
-          <div className={styles.popupButtons}>
-            <button onClick={() => setShowQuickMealPanel(false)}>
-              ZAMKNIJ
-            </button>
-            <button onClick={handleGenerateMeal}>GENERUJ POSIŁEK</button>
-          </div>
+    {showQuickMealPanel && (
+      <div className={styles.popupWrapper}>
+        <textarea
+          className={styles.popupTextarea}
+          value={quickMealInput}
+          onChange={e => setQuickMealInput(e.target.value)}
+        />
+        <div className={styles.popupButtons}>
+          <button onClick={() => setShowQuickMealPanel(false)}>ZAMKNIJ</button>
+          <button onClick={handleGenerateMeal}>GENERUJ POSIŁEK</button>
         </div>
-      )}
+      </div>
+    )}
 
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            {product.name}
-            <button onClick={() => handleDeleteProduct(product.id)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    {showPantry && (
+      <div className={styles.pantryContainer}>
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              {product.name}
+              <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={togglePantry}>ZAMKNIJ SPICHLERZ</button>
+      </div>
+    )}
 
-export default App;
+    {showMealPlan && (
+      <div className={styles.mealPlanContainer}>
+        <button onClick={toggleMealPlan}>ZAMKNIJ JADŁOSPISY</button>
+      </div>
+    )}
+  </div>
+	)
+}
+
+export default App
--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,49 +1,51 @@
-import { create } from "zustand";
+import { create } from 'zustand'
 
 interface Product {
-  id: number;
-  name: string;
+	id: number
+	name: string
 }
 
 interface Result {
-  id: string;
-  message: string;
+	id: string
+	message: string
 }
 
 interface StoreProps {
-  inputProduct: string;
-  setInputProduct: (inputProduct: string) => void;
-  products: Product[];
-  setProducts: (
-    products: Product[] | ((prevProducts: Product[]) => Product[])
-  ) => void;
-  showQuickMealPanel: boolean;
-  setShowQuickMealPanel: (showQuickMealPanel: boolean) => void;
-  quickMealInput: string;
-  setQuickMealInput: (quickMealInput: string) => void;
-  results: Result[];
-  setResult: (result: Result[] | ((prevResults: Result[]) => Result[])) => void;
+	inputProduct: string
+	setInputProduct: (inputProduct: string) => void
+	products: Product[]
+	setProducts: (products: Product[] | ((prevProducts: Product[]) => Product[])) => void
+	showQuickMealPanel: boolean
+	setShowQuickMealPanel: (showQuickMealPanel: boolean) => void
+	quickMealInput: string
+	setQuickMealInput: (quickMealInput: string) => void
+	results: Result[]
+	setResult: (result: Result[] | ((prevResults: Result[]) => Result[])) => void
+	showPantry: boolean
+	setShowPantry: (showPantry: boolean) => void
+	showMealPlan: boolean
+	setShowMealPlan: (showMealPlan: boolean) => void
 }
 
-export const useStore = create<StoreProps>((set) => ({
-  inputProduct: "",
-  setInputProduct: (inputProduct: string) => set({ inputProduct }),
-  products: [],
-  setProducts: (
-    products: Product[] | ((prevProducts: Product[]) => Product[])
-  ) =>
-    set((state) => ({
-      products:
-        typeof products === "function" ? products(state.products) : products,
-    })),
-  showQuickMealPanel: false,
-  setShowQuickMealPanel: (showQuickMealPanel: boolean) =>
-    set({ showQuickMealPanel }),
-  quickMealInput: "",
-  setQuickMealInput: (quickMealInput: string) => set({ quickMealInput }),
-  results: [],
-  setResult: (results: Result[] | ((prevResults: Result[]) => Result[])) =>
-    set((state) => ({
-      results: typeof results === "function" ? results(state.results) : results,
-    })),
-}));
+export const useStore = create<StoreProps>(set => ({
+	inputProduct: '',
+	setInputProduct: (inputProduct: string) => set({ inputProduct }),
+	products: [],
+	setProducts: (products: Product[] | ((prevProducts: Product[]) => Product[])) =>
+		set(state => ({
+			products: typeof products === 'function' ? products(state.products) : products,
+		})),
+	showQuickMealPanel: false,
+	setShowQuickMealPanel: (showQuickMealPanel: boolean) => set({ showQuickMealPanel }),
+	quickMealInput: '',
+	setQuickMealInput: (quickMealInput: string) => set({ quickMealInput }),
+	results: [],
+	setResult: (results: Result[] | ((prevResults: Result[]) => Result[])) =>
+		set(state => ({
+			results: typeof results === 'function' ? results(state.results) : results,
+		})),
+	showPantry: false,
+	setShowPantry: (showPantry: boolean) => set({ showPantry }),
+	showMealPlan: false,
+	setShowMealPlan: (showMealPlan: boolean) => set({ showMealPlan }),
+}))
