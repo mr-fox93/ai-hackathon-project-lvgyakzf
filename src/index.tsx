6e2f3de6883a937f234fa36c5720fd7de1449096
hansac91 <saczukhanna@gmail.com>
2024-04-28 17:16:23 +0200

    pwa implement

--- src/index.tsx
+++ src/index.tsx
@@ -17,3 +17,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('Service Worker registration failed: ', registrationError);
+      });
+  });
+}
+
