98f1d251d82d29d17aa638d4d7e92b4e10725759
AniaPiWo <snowflake@wp.pl>
2024-04-28 12:05:07 +0200

    ui part 1

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
-import styles from './Checkbox.module.css'; // Importuj styl CSS dla checkboxa
+import React, { useState } from "react";
+import styles from "./Checkbox.module.css"; // Importuj styl CSS dla checkboxa
 
 interface CheckboxProps {
-    id: string;
-    label: string;
-    checked: boolean;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    disabled?: boolean;
+  id: string;
+  label: string;
+  checked: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
 }
 
-const Checkbox = ({ id, label, checked= true, onChange, disabled }: CheckboxProps) => {
+const Checkbox = ({
+  id,
+  label,
+  checked = true,
+  onChange,
+  disabled,
+}: CheckboxProps) => {
   const [isChecked, setIsChecked] = useState(checked);
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,18 +24,25 @@ const Checkbox = ({ id, label, checked= true, onChange, disabled }: CheckboxProp
   };
 
   return (
-    <label htmlFor={id} className={styles.label} onClick={() => handleCheckboxChange}>
-      <input 
-        id={id} 
-        type="checkbox" 
-        checked={isChecked} 
-        onChange={handleCheckboxChange} 
-        disabled={disabled} 
-        className={styles.checkbox} // Upewnij się, że nazwa klasy jest poprawna
-      />
-      <div className={styles.customCheckbox}></div> {/* Użyj poprawnej nazwy klasy dla Twojego niestandardowego wyglądu */}
-      {label}
-    </label>
+    <div className={styles.container}>
+      <label
+        htmlFor={id}
+        className={styles.label}
+        onClick={() => handleCheckboxChange}
+      >
+        <input
+          id={id}
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          disabled={disabled}
+          className={styles.checkbox} // Upewnij się, że nazwa klasy jest poprawna
+        />
+        <div className={styles.customCheckbox}></div>{" "}
+        {/* Użyj poprawnej nazwy klasy dla Twojego niestandardowego wyglądu */}
+        {label}
+      </label>
+    </div>
   );
 };
 
