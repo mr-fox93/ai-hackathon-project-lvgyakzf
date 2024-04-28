fcc132c855a49cd1eb01b9e4324fe6a3b9496338
The Fox <lisieckikamil93@gmail.com>
2024-04-28 11:47:01 +0200

    feat: checking we can make a recipe in favorite with granary ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,259 +1,312 @@
-import React, { useEffect, useState } from 'react'
-import { fetchChatCompletion } from './apiService'
-import styles from './app.module.css'
+import React, { useEffect, useState } from "react";
+import { fetchChatCompletion } from "./apiService";
+import styles from "./app.module.css";
 import {
-	addProduct,
-	getProducts,
-	removeProduct,
-	deleteDatabase,
-	getMealPlans,
-	addMealPlan,
-	removeMealPlan,
-} from './database'
-import { useStore } from './store/useStore'
-import SpeechToText from './SpeechTotext'
-import { knownProducts } from './knownProducts'
-import Checkbox from './components/Checkbox/Checkbox'
-import Loader from './components/Loader/Loader'
+  addProduct,
+  getProducts,
+  removeProduct,
+  deleteDatabase,
+  getMealPlans,
+  addMealPlan,
+  removeMealPlan,
+} from "./database";
+import { useStore } from "./store/useStore";
+import SpeechToText from "./SpeechTotext";
+import { knownProducts } from "./knownProducts";
+import Checkbox from "./components/Checkbox/Checkbox";
+import Loader from "./components/Loader/Loader";
 
 interface MealPlan {
-	id: number
-	content: string
+  id: number;
+  content: string;
 }
 
 const App: React.FC = () => {
-	const [loading, setLoading] = useState(false)
-	const [response, setResponse] = useState('')
-	const [mealPlans, setMealPlans] = useState([] as MealPlan[])
-	const [showProductNames, setShowProductNames] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState("");
+  const [mealPlans, setMealPlans] = useState([] as MealPlan[]);
+  const [showProductNames, setShowProductNames] = useState(false);
 
-	useEffect(() => {
-		getMealPlans()
-	}, [])
+  useEffect(() => {
+    getMealPlans();
+  }, []);
 
-	const {
-		inputProduct,
-		setInputProduct,
-		products,
-		setProducts,
-		quickMealInput,
-		setQuickMealInput,
-		setResult,
-		results,
-		showMealPlan,
-		setShowMealPlan,
-		showPantry,
-		setShowPantry,
-	} = useStore()
+  const {
+    inputProduct,
+    setInputProduct,
+    products,
+    setProducts,
+    quickMealInput,
+    setQuickMealInput,
+    setResult,
+    results,
+    showMealPlan,
+    setShowMealPlan,
+    showPantry,
+    setShowPantry,
+  } = useStore();
 
-	const normalizeText = (text: string) =>
-		text
-			.normalize('NFD')
-			.replace(/[\u0300-\u036f]/g, '')
-			.toLowerCase()
+  const normalizeText = (text: string) =>
+    text
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase();
 
-	useEffect(() => {
-		fetchProducts()
-	}, [])
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-	useEffect(() => {
-		loadMealPlans()
-	}, [])
+  useEffect(() => {
+    loadMealPlans();
+  }, []);
 
-	useEffect(() => {
-		console.log(results)
-	}, [results])
+  useEffect(() => {
+    console.log(results);
+  }, [results]);
 
-	const fetchProducts = async () => {
-		const items = await getProducts()
-		setProducts(items)
-	}
+  const fetchProducts = async () => {
+    const items = await getProducts();
+    setProducts(items);
+  };
 
-	const handleAddProduct = async () => {
-		let inputLower = normalizeText(quickMealInput)
-		const productsToAdd = []
+  const handleAddProduct = async () => {
+    let inputLower = normalizeText(quickMealInput);
+    const productsToAdd = [];
 
-		// Check for known products and add them separately
-		for (const product of knownProducts) {
-			const normalizedProduct = normalizeText(product)
-			if (inputLower.includes(normalizedProduct)) {
-				productsToAdd.push(product) // Add the original name, not normalized
-				inputLower = inputLower.replace(new RegExp(normalizedProduct, 'g'), '')
-			}
-		}
+    // Check for known products and add them separately
+    for (const product of knownProducts) {
+      const normalizedProduct = normalizeText(product);
+      if (inputLower.includes(normalizedProduct)) {
+        productsToAdd.push(product); // Add the original name, not normalized
+        inputLower = inputLower.replace(new RegExp(normalizedProduct, "g"), "");
+      }
+    }
 
-		// Add remaining single word products
-		inputLower.split(' ').forEach(word => {
-			if (word.trim() !== '') productsToAdd.push(word)
-		})
+    // Add remaining single word products
+    inputLower.split(" ").forEach((word) => {
+      if (word.trim() !== "") productsToAdd.push(word);
+    });
 
-		// Add each product to the database and state
-		for (const product of productsToAdd) {
-			const newProduct = await addProduct({ name: product })
-			if (newProduct.id) {
-				setProducts(prev => [...prev, newProduct])
-			}
-		}
+    // Add each product to the database and state
+    for (const product of productsToAdd) {
+      const newProduct = await addProduct({ name: product });
+      if (newProduct.id) {
+        setProducts((prev) => [...prev, newProduct]);
+      }
+    }
 
-		setQuickMealInput('')
-	}
+    setQuickMealInput("");
+  };
 
-	const handleDeleteProduct = async (id: IDBValidKey | IDBKeyRange) => {
-		await removeProduct(id)
-		fetchProducts()
-	}
+  const handleDeleteProduct = async (id: IDBValidKey | IDBKeyRange) => {
+    await removeProduct(id);
+    fetchProducts();
+  };
 
-	const togglePantry = () => {
-		setShowPantry(!showPantry)
-	}
+  const togglePantry = () => {
+    setShowPantry(!showPantry);
+  };
 
-	const toggleMealPlan = () => {
-		setShowMealPlan(!showMealPlan)
-	}
+  const toggleMealPlan = () => {
+    setShowMealPlan(!showMealPlan);
+  };
 
-	// PROMPTY I STRZAŁ DO API
-	const handleTranscription = (transcript: string) => {
-		setQuickMealInput(transcript)
-	}
+  // PROMPTY I STRZAŁ DO API
+  const handleTranscription = (transcript: string) => {
+    setQuickMealInput(transcript);
+  };
 
-	const handleClear = () => {
-		setQuickMealInput('')
-		setResponse('')
-	}
+  const handleClear = () => {
+    setQuickMealInput("");
+    setResponse("");
+  };
 
-	const promptQuciky = `Bazując na tych składnikach: ${quickMealInput}, podaj mi prosty i szybki przepis do zrobienia.`
-	// const granaryQuick = `Bazując na tych składnikach: ${inputProduct}, podaj mi prosty i szybki przepis do zrobienia.`;
+  const promptQuciky = `Bazując na tych składnikach: ${quickMealInput}, podaj mi prosty i szybki przepis do zrobienia.`;
+  // const granaryQuick = `Bazując na tych składnikach: ${inputProduct}, podaj mi prosty i szybki przepis do zrobienia.`;
 
-	const handleGenerateMeal = async () => {
-		setLoading(true)
-		await fetchChatCompletion(
-			`Bazując na tych składnikach: ${
-				showProductNames ? quickMealInput + ', ' + products.map(product => product.name).join(', ') : quickMealInput
-			}, podaj mi prosty i szybki przepis do zrobienia.`,
-			(response: React.SetStateAction<string>) => {
-				setResponse(response)
-				setLoading(false)
-			},
-			setLoading
-		)
-	}
+  // const checkToDoAgain = async (plan: string) => {
+  //   setLoading(true);
+  //   await fetchChatCompletion(
+  //     `Czy mogę wykonać ten przepis: ${plan} bazując na składnikach, które mam w moim spichlerzu: ${products}. Sprawdź dokładnie to co mam i czy zgadza się z tym czego wymaga przepis, zwróć krótkie info Mogę lub nie mogę. Jeśli w ${products} nic mie ma zwróć info: 'TWÓJ SPICHLERZ JEST PUSTY MÓJ PANIE`,
+  //     (response: React.SetStateAction<string>) => {
+  //       // setResponse(response)
+  //       alert(response);
+  //       setLoading(false);
+  //     },
+  //     setLoading
+  //   );
+  // };
 
-	const handleGenerateMealPlan = async () => {
-		setLoading(true)
-		await fetchChatCompletion(
-			`Bazując na tych składnikach: ${
-				showProductNames ? quickMealInput + ', ' + products.map(product => product.name).join(', ') : quickMealInput
-			}, podaj mi jadłospis na cały dzień. jeśli składników jest za mało zasugeruj coś na zakupy`,
-			(response: React.SetStateAction<string>) => {
-				setResponse(response)
-				setLoading(false)
-			},
-			setLoading
-		)
-	}
+  const checkToDoAgain = async (plan: string) => {
+    setLoading(true);
+    const products = await getProducts(); // Pobierz produkty za pomocą funkcji getProducts
 
-	const handleUptadeTextArea = (value: string) => {
-		setQuickMealInput(value)
-		setInputProduct(value)
-	}
+    if (products.length === 0) {
+      alert("TWÓJ SPICHLERZ JEST PUSTY MÓJ PANIE");
+      setLoading(false);
+      return;
+    }
 
-	const handleAddToFavorites = async () => {
-		if (response) {
-			const newMealPlan = await addMealPlan(response)
-			setMealPlans(prev => [...prev, newMealPlan])
-		}
-	}
+    const productNames = products.map((product) => product.name).join(", "); // Przyjmijmy, że każdy produkt ma pole 'name'
 
-	const loadMealPlans = async () => {
-		const loadedMealPlans = await getMealPlans()
-		setMealPlans(loadedMealPlans)
-		console.log('Meal plans loaded:', loadedMealPlans)
-	}
+    await fetchChatCompletion(
+      `Czy mogę wykonać ten przepis: ${plan} bazując na składnikach, które mam w moim spichlerzu: ${productNames}. Sprawdź dokładnie to co mam i czy zgadza się z tym czego wymaga przepis. Jeśli mam część składników wymień te których nie mam. Jak mam zupełnie inne zaproponuj przepis do zrobienia z tego co mam.`,
+      (response: React.SetStateAction<string>) => {
+        alert(response);
+        setLoading(false);
+      },
+      setLoading
+    );
+  };
 
-	const handleDeleteMealPlan = async (id: IDBValidKey | IDBKeyRange) => {
-		await removeMealPlan(id)
-		loadMealPlans()
-	}
+  const handleGenerateMeal = async () => {
+    setLoading(true);
+    await fetchChatCompletion(
+      `Bazując na tych składnikach: ${
+        showProductNames
+          ? quickMealInput +
+            ", " +
+            products.map((product) => product.name).join(", ")
+          : quickMealInput
+      }, podaj mi prosty i szybki przepis do zrobienia.`,
+      (response: React.SetStateAction<string>) => {
+        setResponse(response);
+        setLoading(false);
+      },
+      setLoading
+    );
+  };
 
-	return (
-		<div className={styles.container}>
-			{loading && <Loader />}
-			{response && !showMealPlan && !showMealPlan && (
-				<div className={styles.responseWrapper}>
-					<p className={styles.response}>{response}</p>
-					<button onClick={handleAddToFavorites}>Add to Favorites</button>
-				</div>
-			)}
-			{!showPantry && !showMealPlan && (
-				<div className={styles.actionsWrapper}>
-					<button onClick={togglePantry}>SPICHLERZ</button>
-					<button onClick={toggleMealPlan}>JADŁOSPISY</button>
-				</div>
-			)}
+  const handleGenerateMealPlan = async () => {
+    setLoading(true);
+    await fetchChatCompletion(
+      `Bazując na tych składnikach: ${
+        showProductNames
+          ? quickMealInput +
+            ", " +
+            products.map((product) => product.name).join(", ")
+          : quickMealInput
+      }, podaj mi jadłospis na cały dzień. jeśli składników jest za mało zasugeruj coś na zakupy`,
+      (response: React.SetStateAction<string>) => {
+        setResponse(response);
+        setLoading(false);
+      },
+      setLoading
+    );
+  };
 
-			{!showPantry && !showMealPlan && (
-				<div className={styles.mainWrapper}>
-					<SpeechToText onTranscript={handleTranscription} onClear={handleClear} />
-					<textarea
-						className={styles.textarea}
-						value={quickMealInput}
-						onChange={e => handleUptadeTextArea(e.target.value)}
-						placeholder='Enter products...'
-					/>
-					<button className={styles.addButton} onClick={handleAddProduct}>
-						DODAJ DO SPICHLERZA
-					</button>
-					<div className={styles.buttonsWrapper}>
-						<button onClick={handleGenerateMeal}>SZYBKIE JEDZONKO</button>
-						<button onClick={handleGenerateMealPlan}>KONKRETNE JEDZONKO</button>
-					</div>
-					<Checkbox
-						id='showProductNames'
-						label='uwzględnij spichlerz'
-						checked={showProductNames}
-						onChange={e => setShowProductNames(e.target.checked)}
-					/>
-				</div>
-			)}
+  const handleUptadeTextArea = (value: string) => {
+    setQuickMealInput(value);
+    setInputProduct(value);
+  };
 
-			{showPantry && (
-				<div className={styles.pantryContainer}>
-					<ul className={styles.pantryList}>
-						{products.map(product => (
-							<li key={product.id}>
-								{product.name}
-								<button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
-							</li>
-						))}
-					</ul>
-					<div className={styles.pantryBtnWrapper}>
-						<button className={styles.pantryBtn} onClick={togglePantry}>
-							ZAMKNIJ SPICHLERZ
-						</button>
-						<button className={styles.pantryBtn} onClick={deleteDatabase}>
-							CLEAR ALL
-						</button>
-					</div>
-				</div>
-			)}
+  const handleAddToFavorites = async () => {
+    if (response) {
+      const newMealPlan = await addMealPlan(response);
+      setMealPlans((prev) => [...prev, newMealPlan]);
+    }
+  };
 
-			{showMealPlan && (
-				<div className={styles.mealPlanContainer}>
-					<button className={styles.closeMealsBtn} onClick={toggleMealPlan}>
-						ZAMKNIJ JADŁOSPISY
-					</button>
-					<div className={styles.flexWrapper}>
-						{mealPlans.map((plan, index) => (
-							<div className={styles.mealWrapper} key={index}>
-								<p>{plan.content}</p>
-								<button onClick={() => handleDeleteMealPlan(plan.id)}>Delete</button>
-							</div>
-						))}
-					</div>
-				</div>
-			)}
-		</div>
-	)
-}
+  const loadMealPlans = async () => {
+    const loadedMealPlans = await getMealPlans();
+    setMealPlans(loadedMealPlans);
+    console.log("Meal plans loaded:", loadedMealPlans);
+  };
 
-export default App
+  const handleDeleteMealPlan = async (id: IDBValidKey | IDBKeyRange) => {
+    await removeMealPlan(id);
+    loadMealPlans();
+  };
+
+  return (
+    <div className={styles.container}>
+      {loading && <Loader />}
+      {response && !showMealPlan && !showMealPlan && (
+        <div className={styles.responseWrapper}>
+          <p className={styles.response}>{response}</p>
+          <button onClick={handleAddToFavorites}>Add to Favorites</button>
+        </div>
+      )}
+      {!showPantry && !showMealPlan && (
+        <div className={styles.actionsWrapper}>
+          <button onClick={togglePantry}>SPICHLERZ</button>
+          <button onClick={toggleMealPlan}>JADŁOSPISY</button>
+        </div>
+      )}
+
+      {!showPantry && !showMealPlan && (
+        <div className={styles.mainWrapper}>
+          <SpeechToText
+            onTranscript={handleTranscription}
+            onClear={handleClear}
+          />
+          <textarea
+            className={styles.textarea}
+            value={quickMealInput}
+            onChange={(e) => handleUptadeTextArea(e.target.value)}
+            placeholder="Enter products..."
+          />
+          <button className={styles.addButton} onClick={handleAddProduct}>
+            DODAJ DO SPICHLERZA
+          </button>
+          <div className={styles.buttonsWrapper}>
+            <button onClick={handleGenerateMeal}>SZYBKIE JEDZONKO</button>
+            <button onClick={handleGenerateMealPlan}>KONKRETNE JEDZONKO</button>
+          </div>
+          <Checkbox
+            id="showProductNames"
+            label="uwzględnij spichlerz"
+            checked={showProductNames}
+            onChange={(e) => setShowProductNames(e.target.checked)}
+          />
+        </div>
+      )}
+
+      {showPantry && (
+        <div className={styles.pantryContainer}>
+          <ul className={styles.pantryList}>
+            {products.map((product) => (
+              <li key={product.id}>
+                {product.name}
+                <button onClick={() => handleDeleteProduct(product.id)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.pantryBtnWrapper}>
+            <button className={styles.pantryBtn} onClick={togglePantry}>
+              ZAMKNIJ SPICHLERZ
+            </button>
+            <button className={styles.pantryBtn} onClick={deleteDatabase}>
+              CLEAR ALL
+            </button>
+          </div>
+        </div>
+      )}
+
+      {showMealPlan && (
+        <div className={styles.mealPlanContainer}>
+          <button className={styles.closeMealsBtn} onClick={toggleMealPlan}>
+            ZAMKNIJ JADŁOSPISY
+          </button>
+          <div className={styles.flexWrapper}>
+            {mealPlans.map((plan, index) => (
+              <div className={styles.mealWrapper} key={index}>
+                <p>{plan.content}</p>
+                <button onClick={() => checkToDoAgain(plan.content)}>
+                  Zrób Ponoenir
+                </button>
+                <button onClick={() => handleDeleteMealPlan(plan.id)}>
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
--- src/database.ts
+++ src/database.ts
@@ -1,97 +1,109 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
-const DATABASE_NAME = 'FoodInventoryDBNew';
-const STORE_NAME = 'products';
-const MEAL_PLAN_STORE_NAME = 'mealPlans';
+const DATABASE_NAME = "FoodInventoryDBNew";
+const STORE_NAME = "products";
+const MEAL_PLAN_STORE_NAME = "mealPlans";
 const VERSION = 1;
 
 async function initDB() {
-    const db = await openDB(DATABASE_NAME, VERSION, {
-      upgrade(db, oldVersion, newVersion, transaction) {
-        if (!db.objectStoreNames.contains(STORE_NAME)) {
-          db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
-        }
-        if (!db.objectStoreNames.contains(MEAL_PLAN_STORE_NAME)) {
-          db.createObjectStore(MEAL_PLAN_STORE_NAME, { keyPath: 'id', autoIncrement: true });
-        }
-      },
-    });
-    return db;
+  const db = await openDB(DATABASE_NAME, VERSION, {
+    upgrade(db, oldVersion, newVersion, transaction) {
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      }
+      if (!db.objectStoreNames.contains(MEAL_PLAN_STORE_NAME)) {
+        db.createObjectStore(MEAL_PLAN_STORE_NAME, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      }
+    },
+  });
+  return db;
 }
 
 export async function addProduct(product: any) {
-    const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readwrite');
-    const store = tx.objectStore(STORE_NAME);
-    const id = await store.add(product); // Zwracane ID powinno być typu number
-    await tx.done;
-    console.log("Added product with ID:", id);
-    return { ...product, id }; // Zwróć produkt z dodanym id
-  }
-
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, "readwrite");
+  const store = tx.objectStore(STORE_NAME);
+  const id = await store.add(product); // Zwracane ID powinno być typu number
+  await tx.done;
+  console.log("Added product with ID:", id);
+  return { ...product, id }; // Zwróć produkt z dodanym id
+}
 
 // POKAŻ PRODUKTY
 export async function getProducts() {
-    const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readonly');
-    const store = tx.objectStore(STORE_NAME);
-    const products = await store.getAll();
-    console.log("Retrieved products:", products); 
-    return products;
-  }
-
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, "readonly");
+  const store = tx.objectStore(STORE_NAME);
+  const products = await store.getAll();
+  console.log("Retrieved products:", products);
+  return products;
+}
+// POKAŻ SKŁADNIKI
+// export async function getProducts() {
+//   const db = await initDB();
+//   const tx = db.transaction("ingredientsStore", "readonly"); // Załóżmy, że składniki są przechowywane w 'ingredientsStore'
+//   const store = tx.objectStore("ingredientsStore");
+//   const ingredients = await store.getAll();
+//   console.log("Retrieved ingredients:", ingredients);
+//   return ingredients;
+// }
 
 //WYPIERDOL PRODUKT
 export async function removeProduct(id: IDBKeyRange | IDBValidKey) {
-    const db = await initDB();
-    const tx = db.transaction(STORE_NAME, 'readwrite');
-    const store = tx.objectStore(STORE_NAME);
-    await store.delete(id);
-    await tx.done;
-  }
-
-  //wypierdol baze
-  // Funkcja do usuwania całej bazy danych
-export async function deleteDatabase() {
-    await openDB(DATABASE_NAME).then(db => {
-      db.close();
-      const deleteDBRequest = indexedDB.deleteDatabase(DATABASE_NAME);
-      deleteDBRequest.onerror = function(event) {
-        console.error("Error deleting database.");
-      };
-      deleteDBRequest.onsuccess = function(event) {
-        console.log("Database deleted successfully");
-      };
-    });
-  }
-  
-  export async function addMealPlan(mealPlanContent: string) {
-    const db = await initDB();
-    const tx = db.transaction(MEAL_PLAN_STORE_NAME, 'readwrite');
-    const store = tx.objectStore(MEAL_PLAN_STORE_NAME);
-    const id = await store.add({ content: mealPlanContent });
-    await tx.done;
-    console.log("Added meal plan with ID:", id);
-    // Explicitly cast id to number
-    return { content: mealPlanContent, id: id as number };
+  const db = await initDB();
+  const tx = db.transaction(STORE_NAME, "readwrite");
+  const store = tx.objectStore(STORE_NAME);
+  await store.delete(id);
+  await tx.done;
 }
 
+//wypierdol baze
+// Funkcja do usuwania całej bazy danych
+export async function deleteDatabase() {
+  await openDB(DATABASE_NAME).then((db) => {
+    db.close();
+    const deleteDBRequest = indexedDB.deleteDatabase(DATABASE_NAME);
+    deleteDBRequest.onerror = function (event) {
+      console.error("Error deleting database.");
+    };
+    deleteDBRequest.onsuccess = function (event) {
+      console.log("Database deleted successfully");
+    };
+  });
+}
+
+export async function addMealPlan(mealPlanContent: string) {
+  const db = await initDB();
+  const tx = db.transaction(MEAL_PLAN_STORE_NAME, "readwrite");
+  const store = tx.objectStore(MEAL_PLAN_STORE_NAME);
+  const id = await store.add({ content: mealPlanContent });
+  await tx.done;
+  console.log("Added meal plan with ID:", id);
+  // Explicitly cast id to number
+  return { content: mealPlanContent, id: id as number };
+}
 
 export async function getMealPlans() {
-    const db = await initDB();
-    const tx = db.transaction(MEAL_PLAN_STORE_NAME, 'readonly');
-    const store = tx.objectStore(MEAL_PLAN_STORE_NAME);
-    const mealPlans = await store.getAll();
-    console.log("Retrieved meal plans:", mealPlans);
-    return mealPlans;
+  const db = await initDB();
+  const tx = db.transaction(MEAL_PLAN_STORE_NAME, "readonly");
+  const store = tx.objectStore(MEAL_PLAN_STORE_NAME);
+  const mealPlans = await store.getAll();
+  console.log("Retrieved meal plans:", mealPlans);
+  return mealPlans;
 }
 
 export async function removeMealPlan(id: IDBValidKey | IDBKeyRange) {
-    console.log("Attempting to remove meal plan with ID:", id);
-    const db = await initDB();
-    const tx = db.transaction(MEAL_PLAN_STORE_NAME, 'readwrite');
-    const store = tx.objectStore(MEAL_PLAN_STORE_NAME);
-    await store.delete(id);
-    await tx.done;
-    console.log("Meal plan removed successfully");
+  console.log("Attempting to remove meal plan with ID:", id);
+  const db = await initDB();
+  const tx = db.transaction(MEAL_PLAN_STORE_NAME, "readwrite");
+  const store = tx.objectStore(MEAL_PLAN_STORE_NAME);
+  await store.delete(id);
+  await tx.done;
+  console.log("Meal plan removed successfully");
 }
